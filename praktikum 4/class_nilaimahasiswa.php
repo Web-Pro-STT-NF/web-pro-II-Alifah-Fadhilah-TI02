47404fdfd3452ef9dc518e79aeae7979ed972455
Alifah Fadhilah <91960361+alifahfadhilah@users.noreply.github.com>
2022-05-10 18:23:47 +0700

    Add files via upload

---
+++ praktikum 4/class_nilaimahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+class NilaiMahasiswa {
+    var $matakuliah;
+    var $nilai;
+    var $nim;
+
+    function __construct ($matkul, $nilai, $nim) {
+        // LENGKAPI
+    }
+
+    function grade () {
+        // LENGKAPI
+    }
+
+    function hasil () {
+        //LENGKAPI
+    }
+}
+?>
